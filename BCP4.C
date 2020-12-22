39fd21d24f2fb6743d6f53be9d3f508b345830ca
7Roshani <74301621+7Roshani@users.noreply.github.com>
2020-12-22 16:49:47 +0530

    BCP
    Write a C program to print the result of the specified operations.
    -1 + 4 * 6
    ( 35+ 5 ) % 7
    14 + -4 * 6 / 11
    2 + 15 / 6 * 1 - 7 % 2

---
+++ BCP4.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int c;
+  clrscr();
+  printf("printf result of %d",-1+4*6);
+  c=35+5;
+  printf("\n2 operation %d",c%7);
+  printf("\n 3 operation %d",14+-4*6/11);
+  printf("\n 4 operation %d",2+15/6*1-7%2);
+  getch();
+}
