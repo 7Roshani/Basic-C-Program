cfdfc1435daf33ee9697c3bc034a85fe192244ad
7Roshani <74301621+7Roshani@users.noreply.github.com>
2020-12-22 16:54:13 +0530

    BCP
    Write a C program that takes four numbers as input to calculate and print the average.

---
+++ BCP9.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int n1,n2,n3,n4,c;
+  clrscr();
+  printf("Please enter 4 no");
+  scanf("%d%d%d%d",&n1,&n2,&n3,&n4);
+  c=(n1+n2+n3+n4)/4;
+  printf("\nAverage:-%d",c);
+  getch()
+}
