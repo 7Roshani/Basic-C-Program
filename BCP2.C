8e273d848df822150a4593e34793fe072ff4124f
7Roshani <74301621+7Roshani@users.noreply.github.com>
2020-12-22 16:48:29 +0530

    BCP
    Write a C program to print the sum of two numbers.

---
+++ BCP2.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int num1, num2,c;
+  clrscr();
+  printf("Plese enter any 2 number\n");
+  scanf("%d%d",&num1,&num2);
+  c=num1+num2;
+  printf("Addition of 2 number:-%d\n",c);
+  printf("\n addition:- %d",num1+num2);
+  getch();
+}
