7026c5ee640065a6782fa06acbc0a46cb85b4a4a
7Roshani <74301621+7Roshani@users.noreply.github.com>
2020-12-22 16:56:06 +0530

    BCP
    Sharp program that takes an age (for example 20) as input and prints something as "You look older than 20".

---
+++ BCP11.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int age;
+  clrscr();
+  printf("Please enter age");
+  scanf("%d",&age);
+  printf("u look older than %d",age);
+  getch();
+}
