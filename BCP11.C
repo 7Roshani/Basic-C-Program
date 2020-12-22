f9cee643cc7991022a46f5571bf72edbfe053413
7Roshani <74301621+7Roshani@users.noreply.github.com>
2020-12-22 17:16:53 +0530

    BCP
    Write a C to convert from celsius degrees to Kelvin and Fahrenheit.

--- BCP11.C
+++ BCP11.C
@@ -2,10 +2,15 @@
 #include<conio.h>
 void main()
 {
-  int age;
+  float K,C;
   clrscr();
-  printf("Please enter age");
-  scanf("%d",&age);
-  printf("u look older than %d",age);
+  printf("Please enter temperature in kelvin");
+  scanf("%f",&K);
+  printf("Please enter temperature in celsius");
+  scanf("%f",&C);
+  K=C+273.15;
+  C=K-273.15;
+  printf("if u eneter Temp. in kelvin then Temp. in celsius is:-%f\n",C);
+  printf("if u eneter Temp. in celsius then Temp. in celsius is:-%f\n",K);
   getch();
 }
