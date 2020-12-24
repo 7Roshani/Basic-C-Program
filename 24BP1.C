63995ad9dd85400b6b7c1544537ad9c32f998069
7Roshani <74301621+7Roshani@users.noreply.github.com>
2020-12-24 16:21:57 +0530

    Update 24BP1.C

--- 24BP1.C
+++ 24BP1.C
@@ -5,14 +5,14 @@ void main()
    int no[10],i,n;
    clrscr();
    printf("Please enter 10 natural numbers");
-   for(i=0;i<=10;i++)
+   for(i=0;i<10;i++)
    {
       scanf("%d",&no[i]);
    }
-   for(i=0;i<=10;i++)
+   printf("10 natural number\n");
+   for(i=0;i<10;i++)
    {
-      n=no[i]+i;
+       printf("%d\n",no[i]);
    }
-   printf("addition on first 10 natural no is :- %d",n);
    getch();
 }
