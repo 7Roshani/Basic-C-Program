29e56dde46421b91f96c81545b672b9058208331
7Roshani <74301621+7Roshani@users.noreply.github.com>
2020-12-26 11:28:44 +0530

    BCP
    Write a program in C to display the cube of the number upto given an integer.

---
+++ 24BCP4.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int no;
+	clrscr();
+	printf("Please enetr no");
+	scanf("%d",&no);
+	printf("cube of given no:-  %d",no*no*no);
+	getch();
+}
