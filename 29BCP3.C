01afa75cac4fbaf9e642d46e630addb8cdb32fa3
7Roshani <74301621+7Roshani@users.noreply.github.com>
2020-12-29 16:48:31 +0530

    BCP
    Write a program in C to copy the elements of one array into another
    array.

--- 29BCP3.C
+++ 29BCP3.C
@@ -2,7 +2,7 @@
 #include<conio.h>
 void main()
 {
-	int a[5],S=0,i;
+	int a[5],c[5],S=0,i;
 	clrscr();
 	printf("Please enter elements in array");
 	for(i=0;i<5;i++)
@@ -14,11 +14,17 @@ void main()
 	{
 		printf("\n%d",a[i]);
 	}
-	printf("\nAddition of all elements \n");
+	printf("\n Addition of all elements \n");
 	for(i=0;i<5;i++)
 	{
 		S=S+a[i];
 	}
 	printf("%d",S);
+	printf("\n Now copy array\n");
+	for(i=0;i<5;i++)
+	{
+		c[i]=a[i];
+		printf("%d\n",c[i]);
+	}
 	getch();
 }
