11f09e5022861152a4970e047a396ad7f2fa4e31
7Roshani <74301621+7Roshani@users.noreply.github.com>
2021-01-14 11:53:47 +0530

    BCP
    WCP that accepts 2 item's weight and calculate the average value of item.

---
+++ 2511BCP1.C
@@ -0,0 +1,12 @@
+	#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	float W,A=0.00;
+	clrscr();
+	printf("Please enter weight of 2 iteam in Kg");
+	scanf("%f",&W);
+	A=W/2;
+	printf("Average of 2 item %f",A);
+	getch();
+}
